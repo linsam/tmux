af284783128c30e192a90601bf7a737a71cf5ad8
John O'Meara <john.fr.omeara@gmail.com>
2015-05-06 23:37:15 -0400

    Fix change of windows from ARRAY to RB

--- cmd-set-option.c
+++ cmd-set-option.c
@@ -186,8 +186,8 @@ cmd_set_option_exec(struct cmd *self, struct cmd_q *cmdq)
 	/* When the pane-status option has been changed, resize panes. */
 	if (strcmp(oe->name, "pane-status") == 0 ||
 	    strcmp(oe->name, "pane-status-position") == 0) {
-		for (i = 0; i < ARRAY_LENGTH(&windows); i++) {
-			if ((w = ARRAY_ITEM(&windows, i)) == NULL)
+		RB_FOREACH(w, windows, &windows) {
+			if (w == NULL)
 				continue;
 			layout_fix_panes(w, w->sx, w->sy);
 		}
